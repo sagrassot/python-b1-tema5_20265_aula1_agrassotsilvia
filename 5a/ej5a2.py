89648b44c9312236afa95dcace0991e4eb7b4d31
Silvia Agrassot <sagrassot@gmail.com>
2026-05-21 17:03:48 +0200

    aj5a2.py Acabat

--- 5a/ej5a2.py
+++ 5a/ej5a2.py
@@ -72,24 +72,33 @@ from abc import ABC, abstractmethod
 
 # Corret and overwrite class Animal here 
 class Animal(ABC):
-    pass
+    @abstractmethod
+    def make_sound(self):
+        pass
 
 # Corret and overwrite class Dog(Animal) here
-class Dog():
+class Dog(Animal):
+    def make_sound(self):
+        return "Woof"
     pass
 
 
 # Corret and overwrite class class Cat(Animal) here
-class Cat():
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow"
     pass
 
 # Corret and overwrite class Duck(Animal) here
-class Duck():
+class Duck(Animal):
+    def make_sound(self):
+        return "Quack"
     pass
 
 # Create a list of animals here
-animals = []
+animals = [Dog(), Cat(), Duck()]
 # Print animals sounds
 for animal in animals:
-    # Write your code here
+    # Escribim el soroll de l'animal
+    print(f'{animal.make_sound()}')
     pass
